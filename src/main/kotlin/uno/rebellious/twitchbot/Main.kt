3bbc90fdabb9a73571798405d3b6c0f8d1a3e3cf
rebelliousuno <RebelliousUno@gmail.com>
2018-09-20 22:09:53 +0100

    refactor(cmdlist): Refactor Command list to new method

--- src/main/kotlin/uno/rebellious/twitchbot/Main.kt
+++ src/main/kotlin/uno/rebellious/twitchbot/Main.kt
@@ -12,19 +12,16 @@ import com.gikk.twirk.types.users.TwitchUser
 import com.github.kittinunf.fuel.Fuel
 import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.Gson
-import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
 import java.io.IOException
 import java.util.*
-import kotlin.properties.Delegates
 
 
-val props = Properties()
+
+
 val scanner = Scanner(System.`in`).toObservable().share()
 val SETTINGS = Settings()
-val channel: String = "#" + SETTINGS.channel
 val lastFMUrl = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${SETTINGS.lastFMUser}&api_key=${SETTINGS.lastFMAPI}&format=json&limit=1"
-//val twirkConnections= HashMap<String, Twirk>()
 var threadList = HashMap<String, Thread>()
 val database = DatabaseDAO()
 
@@ -46,11 +43,11 @@ fun startTwirkForChannel(channel: String) {
         twirk.addIrcListener(PatternCommand(twirk, channel))
         twirk.addIrcListener(getOnDisconnectListener(twirk))
 
-        var disposableScanner = scanner
+        val disposableScanner = scanner
             .subscribe {
                 twirk.channelMessage(it)
             }
-        var interupted = false
+        var interupted: Boolean
         do {
             interupted = Thread.currentThread().isInterrupted
             if (interupted) {
@@ -86,8 +83,17 @@ class UnoBotBase constructor(val twirk: Twirk) : TwirkListener {
     }
 }
 
-class Command (val command: String, val helpString: String, val action: (List<String>) -> Any){
+data class Permission(val isOwnerOnly: Boolean, val isModOnly: Boolean, val isSubOnly: Boolean)
 
+class Command (val command: String, val helpString: String, val permissions: Permission, val action: (List<String>) -> Any){
+
+    fun canUseCommand(sender: TwitchUser): Boolean {
+        println(sender.toString())
+        if (permissions.isOwnerOnly && !sender.isOwner) return false
+        if (permissions.isModOnly && !(sender.isMod || sender.isOwner)) return false
+        if (permissions.isSubOnly && !(sender.isOwner || sender.isMod || sender.isSub)) return false
+        return true
+    }
 }
 
 
@@ -96,17 +102,97 @@ class PatternCommand constructor(val twirk: Twirk, val channel: String) : TwirkL
     private var jackboxCode = "NO ROOM CODE SET"
     private var commandList = ArrayList<Command>()
     init {
-        val songCommand = Command("!song", "The last song listened to by $channel") {
-            Fuel.get(lastFMUrl).responseString { _, _, result ->
-                val resultJson: String = result.get().replace("#", "")
-                val json = gson.fromJson<LastFMResponse>(resultJson)
-                val artist = json.recenttracks.track[0].artist.text
-                val track = json.recenttracks.track[0].name
-                val album = json.recenttracks.track[0].album.text
-                twirk.channelMessage("$channel last listened to $track by $artist from the album $album")
-            }}
-        commandList.add(songCommand)
+        if (channel == "rebelliousuno") commandList.add(songCommand())
+        if (channel == "glazedhambot") commandList.add(addChannelCommand())
+        commandList.add(leaveChannelCommand())
+        commandList.add(listChannelsCommand())
+        commandList.add(addCommand())
+        commandList.add(editCommand())
+        commandList.add(delCommand())
+        commandList.add(commandListCommand())
     }
+
+    private fun commandListCommand(): Command {
+        return Command("!cmdlist", "", Permission(false, false, false)) {
+            val dbCommands = database.getAllCommandList(channel)
+            val configuredCommands = commandList.map {
+                it.command
+            }
+            dbCommands.addAll(configuredCommands)
+            twirk.channelMessage("Command List: $dbCommands")
+        }
+    }
+
+    private fun delCommand(): Command {
+        return Command("!delcmd", "", Permission(false, true, false)) {
+            val removeCommand = it[1].toLowerCase(Locale.ENGLISH)
+            database.removeResponse(channel, removeCommand)
+        }
+    }
+
+    private fun addCommand(): Command {
+        return Command("!addcmd", "", Permission(false, true, false)) {
+            if (it.size > 2) {
+                val newCommand = it[1].toLowerCase(Locale.ENGLISH)
+                val newResponse = it[2]
+                database.setResponse(channel, newCommand, newResponse)
+            }
+        }
+    }
+
+    private fun editCommand(): Command {
+        return Command("!editcmd", "", Permission(false, true, false)) {
+            if (it.size > 2) {
+                val newCommand = it[1].toLowerCase(Locale.ENGLISH)
+                val newResponse = it[2]
+                database.setResponse(channel, newCommand, newResponse)
+            }
+        }
+    }
+
+    private fun addChannelCommand(): Command {
+        return Command("!addchannel",
+            "Usage: !addchannel channeltoAdd - Add a GlazedHamBot to a channel",
+            Permission(false, false, false)
+        ) {
+            if (it.size > 1) {
+                val newChannel = it[1].toLowerCase(Locale.ENGLISH)
+                database.addChannel(newChannel)
+                startTwirkForChannel(newChannel)
+            }
+        }
+    }
+
+    private fun leaveChannelCommand(): Command {
+        return Command("!hamleave", "",
+            Permission(false, true, false)) {
+            database.leaveChannel(channel)
+            twirk.channelMessage("Leaving $channel")
+            stopTwirkForChannel(channel)
+        }
+    }
+
+    private fun songCommand(): Command {
+        return Command("!song",
+            "The last song listened to by $channel",
+            Permission(false, false, false)) {
+            Fuel.get(lastFMUrl).responseString { _, _, result ->
+                    val resultJson: String = result.get().replace("#", "")
+                    val json = gson.fromJson<LastFMResponse>(resultJson)
+                    val artist = json.recenttracks.track[0].artist.text
+                    val track = json.recenttracks.track[0].name
+                    val album = json.recenttracks.track[0].album.text
+                    twirk.channelMessage("$channel last listened to $track by $artist from the album $album")
+            }}
+    }
+
+    private fun listChannelsCommand(): Command {
+        return Command("!listchannels", "", Permission(false, true, false)) {
+            val channelList = database.getListOfChannels()
+            twirk.channelMessage("GlazedHamBot is present in $channelList")
+        }
+    }
+
 
     override fun onPrivMsg(sender: TwitchUser, message: TwitchMessage) {
         val content: String = message.getContent().trim()
@@ -116,57 +202,11 @@ class PatternCommand constructor(val twirk: Twirk, val channel: String) : TwirkL
         val command = splitContent[0].toLowerCase(Locale.ENGLISH)
 
         commandList
-                .firstOrNull { it.command.startsWith(command) }
+                .filter { it.command.startsWith(command) }
+                .firstOrNull { it.canUseCommand(sender) }
                 ?.action?.invoke(splitContent)
-
-
         when {
-//            command.startsWith("!song") && channel=="RebelliousUno".toLowerCase() -> Fuel.get(lastFMUrl).responseString { _, _, result ->
-//                val resultJson: String = result.get().replace("#", "")
-//                val json = gson.fromJson<LastFMResponse>(resultJson)
-//                val artist = json.recenttracks.track[0].artist.text
-//                val track = json.recenttracks.track[0].name
-//                val album = json.recenttracks.track[0].album.text
-//                twirk.channelMessage("$channel last listened to $track by $artist from the album $album")
-//            }
-            command.startsWith("!addchannel")
-                    && (channel.toLowerCase() == "glazedhambot")
-                    && (splitContent.size > 1) -> {
-                val newChannel = splitContent[1].toLowerCase(Locale.ENGLISH)
-                database.addChannel(newChannel)
-                startTwirkForChannel(newChannel)
-            }
-            command.startsWith("!hamleave") && (sender.userType == USER_TYPE.OWNER || sender.displayName.toLowerCase() == "rebelliousuno") -> {
-                database.leaveChannel(channel)
-                twirk.channelMessage("Leaving $channel")
-                stopTwirkForChannel(channel)
-            }
 
-            command.startsWith("!listchannels") && sender.isMod -> {
-                val channelList = database.getListOfChannels()
-                twirk.channelMessage("GlazedHamBot is present in $channelList")
-            }
-
-            command.startsWith("!cmdlist") -> {
-                val dbCommands = database.getAllCommandList(channel)
-                dbCommands.add("!song")
-                dbCommands.add("!jack")
-                twirk.channelMessage("Command List: " + dbCommands)
-                if (sender.isMod || sender.isOwner) {
-                    twirk.channelMessage("Mods Only: !jackset, !addcmd, !editcmd, !delcmd")
-                }
-            }
-            command.startsWith("!addcmd") || command.startsWith("!editcmd")
-                    && splitContent.size > 2 && (sender.isMod || sender.isOwner) -> {
-                val newCommand = splitContent[1].toLowerCase(Locale.ENGLISH)
-                val newResponse = splitContent[2]
-                database.setResponse(channel, newCommand, newResponse)
-            }
-            command.startsWith("!delcmd")
-                    && splitContent.size > 1 && (sender.isMod || sender.isOwner) -> {
-                val removeCommand = splitContent[1].toLowerCase(Locale.ENGLISH)
-                database.removeResponse(channel, removeCommand)
-            }
             command.startsWith("!help") && splitContent.size > 1 -> when {
                 splitContent[1].contains("song") -> twirk.channelMessage("!song - shows most recently played song")
                 splitContent[1].contains("jack") -> {
