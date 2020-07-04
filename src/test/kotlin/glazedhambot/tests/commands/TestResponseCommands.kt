a089feafff7e9c497b4057cdcce9a244bdf968ad
RebelliousUno <RebelliousUno@gmail.com>
2020-07-04 22:13:04 +0100

    test(Response): Add tests response

---
+++ src/test/kotlin/glazedhambot/tests/commands/TestResponseCommands.kt
@@ -0,0 +1,74 @@
+package glazedhambot.tests.commands
+
+import com.gikk.twirk.Twirk
+import com.gikk.twirk.types.users.TwitchUser
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.verifyNoInteractions
+import uno.rebellious.twitchbot.command.ResponseCommands
+import uno.rebellious.twitchbot.database.DatabaseDAO
+import uno.rebellious.twitchbot.model.Response
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TestResponseCommands {
+    lateinit var responseCommands: ResponseCommands
+    lateinit var mockResponseDAO: DatabaseDAO
+    lateinit var mockTwirk: Twirk
+    lateinit var mockTwitchUser: TwitchUser
+    val channel = "test"
+
+    @BeforeEach
+    fun setup() {
+        mockTwirk = Mockito.mock(Twirk::class.java)
+        mockResponseDAO = Mockito.mock(DatabaseDAO::class.java)
+        mockTwitchUser = Mockito.mock(TwitchUser::class.java)
+
+        responseCommands = ResponseCommands("!", mockTwirk, channel, mockResponseDAO)
+    }
+
+    @Test
+    fun testContainsAllCommands() {
+        val commandList = listOf(
+            "addcmd",
+            "editcmd",
+            "delcmd"
+        )
+        Assertions.assertTrue(commandList.containsAll(responseCommands.commandList.map { it.command }))
+    }
+
+    @Test
+    fun testAddResponse() {
+        val command = responseCommands.commandList.first { it.command == "addcmd" }
+        val commandString = "!addcmd cmd This is a test command".split(" ", limit = 3)
+        command.action(mockTwitchUser, commandString)
+        verify(mockResponseDAO).setResponse(channel, Response("cmd", "This is a test command"))
+    }
+
+    @Test
+    fun testAddBadResponse() {
+        val command = responseCommands.commandList.first { it.command == "addcmd" }
+        val commandString = "!addcmd cmd".split(" ", limit = 3)
+        command.action(mockTwitchUser, commandString)
+        verifyNoInteractions(mockResponseDAO)
+    }
+    @Test
+    fun testEditResponse() {
+        val command = responseCommands.commandList.first { it.command == "editcmd" }
+        val commandString = "!editcmd cmd This is a test command".split(" ", limit = 3)
+        command.action(mockTwitchUser, commandString)
+        verify(mockResponseDAO).setResponse(channel, Response("cmd", "This is a test command"))
+    }
+
+    @Test
+    fun testDeleteResponse() {
+        val command = responseCommands.commandList.first { it.command == "delcmd" }
+        val commandString = "!delcmd cmd".split(" ", limit = 3)
+        command.action(mockTwitchUser, commandString)
+        verify(mockResponseDAO).removeResponse(channel, Response("cmd"))
+
+    }
+}
---
+++ src/test/kotlin/glazedhambot/tests/dao/TestResponseDAO.kt
@@ -0,0 +1,101 @@
+package glazedhambot.tests.dao
+
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertTrue
+import uno.rebellious.twitchbot.database.CountersDAO
+import uno.rebellious.twitchbot.database.ResponsesDAO
+import uno.rebellious.twitchbot.model.Response
+import java.sql.Connection
+import java.sql.DriverManager
+import kotlin.test.assertEquals
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.Alphanumeric::class)
+class TestResponseDAO
+{
+    lateinit var responsesDAO: ResponsesDAO
+    private var connectionList: HashMap<String, Connection> = HashMap()
+    lateinit var con: Connection
+    private val channel = "test"
+
+    @BeforeEach
+    fun setupTestDB() {
+        con = DriverManager.getConnection("jdbc:sqlite:$channel.db")
+        connectionList[channel] = con
+        responsesDAO = ResponsesDAO(connectionList)
+        responsesDAO.createResponseTable(connectionList[channel]!!)
+    }
+
+    @AfterEach
+    fun clearDownTestDB() {
+        con.close()
+        con = DriverManager.getConnection("jdbc:sqlite:$channel.db")
+        val dropTableSQL = "drop table IF EXISTS responses"
+        con.createStatement()?.execute(dropTableSQL)
+        con.close()
+    }
+
+    @Test
+    fun testDatabaseSetup() {
+        val nameList = ArrayList(listOf("responses"))
+        val resultList = ArrayList<String>()
+        con.createStatement()
+            .executeQuery("SELECT name FROM SQLITE_MASTER")?.run {
+                while (next()) {
+                    resultList.add(getString(1))
+                }
+            }
+        assert(resultList.containsAll(nameList))
+    }
+
+
+    @Test
+    fun findResponse(){
+        val cmd = "cmd"
+        val response = "This is a test response"
+        responsesDAO.setResponse(channel, Response(cmd, response))
+        val result = responsesDAO.findResponse(channel, Response(cmd))
+        assertEquals(response, result.response)
+    }
+
+    @Test
+    fun findMissingResponse(){
+        val cmd = "cmd"
+        val response = "This is a test response"
+        responsesDAO.setResponse(channel, Response(cmd, response))
+        val result = responsesDAO.findResponse(channel, Response("test"))
+        assertTrue(result.isBlank())
+    }
+
+
+    @Test
+    fun setResponse() {
+        val cmd = "cmd"
+        val response = "This is a test response"
+        responsesDAO.setResponse(channel, Response(cmd, response))
+        val result = responsesDAO.findResponse(channel,Response(cmd))
+        assertEquals(response, result.response)
+    }
+
+    @Test
+    fun updateResponse() {
+        val cmd = "cmd"
+        val response = "This is a test response"
+        val responseEdit = "$response edit"
+        responsesDAO.setResponse(channel, Response(cmd, response))
+        responsesDAO.setResponse(channel, Response(cmd, responseEdit))
+        val result = responsesDAO.findResponse(channel,Response(cmd))
+        assertEquals(responseEdit, result.response)
+    }
+
+    @Test
+    fun removeResponse(){
+        val cmd = "cmd"
+        val response = "This is a test response"
+        responsesDAO.setResponse(channel, Response(cmd, response))
+        responsesDAO.removeResponse(channel, Response(cmd))
+        val result = responsesDAO.findResponse(channel, Response(cmd))
+        assertTrue(result.isBlank())
+    }
+
+}
